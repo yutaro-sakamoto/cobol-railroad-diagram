f59bfc1be5748bac0cf6086cfa5c083843864dfd
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-04-12 06:22:24 +0900

    add: implement the first complete converter

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,45 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 
-//FIELD, CHOICE, SEQ, SYMBOL, REPEAT, REPEAT1, STRING, PATTERN, PREC, PREC_RIGHT, PREC_LEFT, BLANK
-
-fn rule_to_sequence(rule: &JsonValue) -> Sequence {
-    let mut seq = Sequence::default();
+//STRING, PATTERN, PREC, PREC_RIGHT, PREC_LEFT, BLANK
+//CHOICE, SEQ, SYMBOL, REPEAT, REPEAT1, FIELD,
+//
+fn rule_to_node(rule: &JsonValue) -> Box<dyn RailroadNode> {
     if rule["type"] == "SYMBOL" {
-        seq.push(Box::new(NonTerminal::new(rule["name"].to_string())));
+        return Box::new(NonTerminal::new(rule["name"].to_string()));
+    } else if rule["type"] == "FIELD" {
+        return rule_to_node(&rule["content"]);
+    } else if rule["type"] == "SEQ" {
+        let mut seq = Sequence::default();
+        for child in rule["members"].members() {
+            seq.push(Box::new(rule_to_node(child)));
+        }
+        return Box::new(seq);
+    } else if rule["type"] == "REPEAT1" {
+        let inner_node = rule_to_node(&rule["content"]);
+        let repeat_node = rule_to_node(&rule["content"]);
+        return Box::new(Repeat::new(inner_node, repeat_node));
+    } else if rule["type"] == "REPEAT" {
+        let inner_node = Empty;
+        let repeat_node = rule_to_node(&rule["content"]);
+        return Box::new(Repeat::new(inner_node, repeat_node));
+    } else if rule["type"] == "CHOICE" {
+        let mut children = Choice::default();
+        for child in rule["members"].members() {
+            children.push(rule_to_node(child));
+        }
+        return Box::new(children);
+    } else if rule["type"] == "PREC" || rule["type"] == "PREC_RIGHT" || rule["type"] == "PREC_LEFT"
+    {
+        return rule_to_node(&rule["content"]);
+    } else if rule["type"] == "BLANK" {
+        return Box::new(Empty);
+    } else if rule["type"] == "STRING" {
+        return Box::new(Terminal::new(rule["value"].to_string()));
+    } else if rule["type"] == "PATTERN" {
+        return Box::new(Terminal::new(rule["value"].to_string()));
     }
-    seq
+    Box::new(Empty)
 }
 
 fn main() {
@@ -24,7 +55,7 @@ fn main() {
 
     let mut count = 0;
     for (key, rule) in rules.entries() {
-        let seq = rule_to_sequence(rule);
+        let seq = rule_to_node(rule);
         let mut dia = Diagram::new(seq);
         dia.add_element(
             svg::Element::new("style")
